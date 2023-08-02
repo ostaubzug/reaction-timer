0d84f22f19c9e10117fa070f447d6034ba83f60c
oliver <ostaubzug@gmail.com>
2023-08-02 09:55:12 +0200

    added button

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <main class="container">
     <section>
       <h1>Ninja Reaction Timer</h1>
+      <button @click="start">play</button>
     </section>
   </main>
 </template>
@@ -10,6 +11,19 @@
 export default {
   name: "App",
   components: {},
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      console.log(this.delay);
+    },
+  },
 };
 </script>
 
