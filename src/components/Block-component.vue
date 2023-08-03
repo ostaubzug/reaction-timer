3467dfa7017bc244e92dcf85cc9ecc0bc66b3787
oliver <ostaubzug@gmail.com>
2023-08-03 17:08:57 +0200

    addedTimer

--- src/components/Block-component.vue
+++ src/components/Block-component.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="block" v-if="showBlock">click me</div>
+  <div class="block" v-if="showBlock" @click="stopTimer">click me</div>
 </template>
 <script>
 export default {
@@ -7,20 +7,26 @@ export default {
   data() {
     return {
       showBlock: false,
+      timer: null,
+      reactionTime: 0,
     };
   },
   mounted() {
-    console.log("component mounted");
     setTimeout(() => {
       this.showBlock = true;
-      console.log(this.delay);
+      this.startTimer();
     }, this.delay);
   },
-  updated() {
-    console.log("component updated");
-  },
-  unmounted() {
-    console.log("unmounted");
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      });
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+      console.log(this.reactionTime);
+    },
   },
 };
 </script>
